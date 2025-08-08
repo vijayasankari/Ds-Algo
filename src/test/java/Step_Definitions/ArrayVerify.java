94592724a2afb8ee0d035366c0c9f5764d551056
Lynda.com student <you@example.com>
2025-08-07 21:07:48 -0400

    Array Verify Stepdefs

---
+++ src/test/java/Step_Definitions/ArrayVerify.java
@@ -0,0 +1,87 @@
+package Step_Definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ArrayVerify {
+	@Given("User is on the login page")
+	public void user_is_on_the_login_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks on Array from dropdown after logged in application")
+	public void user_clicks_on_array_from_dropdown_after_logged_in_application() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("User is in the Array page")
+	public void user_is_in_the_array_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user clicks {string}")
+	public void user_clicks(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("user should be redirected to  {string} page")
+	public void user_should_be_redirected_to_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("user should be see Try Here button below the content")
+	public void user_should_be_see_try_here_button_below_the_content() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user clicks  on {string} page")
+	public void user_clicks_on_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user clicks on Try editor")
+	public void user_clicks_on_try_editor() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user runs the valid code after reaching Try Editor page with a Run button to test")
+	public void user_runs_the_valid_code_after_reaching_try_editor_page_with_a_run_button_to_test() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("user should see {string}")
+	public void user_should_see(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user runs the in valid code after reaching Try Editor page with a Run button to test")
+	public void user_runs_the_in_valid_code_after_reaching_try_editor_page_with_a_run_button_to_test() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	
+
+	@When("user clicks signout button")
+	public void user_clicks_signout_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("user should signout successfully")
+	public void user_should_signout_successfully() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
